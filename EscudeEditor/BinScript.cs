922fe8f04fbed8ba662fd55c1374933a65ada407
Marcus André <ddtank.marcus@gmail.com>
2018-05-11 07:36:49 -0300

    ...

--- EscudeEditor/BinScript.cs
+++ EscudeEditor/BinScript.cs
@@ -27,8 +27,8 @@ namespace EscudeEditor {
         }
 
         public string Encoder(string String, bool Encode) {
-            string Source = Encode ? fullgana + halfgana : halfgana + fullgana;
-            string Target = Encode ? halfgana + fullgana : fullgana + halfgana;
+            string Source = Encode ? fullgana : halfgana;
+            string Target = Encode ? halfgana : fullgana;
 
             string Result = string.Empty;
             for (int i = 0; i < String.Length; i++) {
